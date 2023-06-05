d3d29ef0f501bc48bff923b130da66de12612c97
NeAnon <anontherper@gmail.com>
2023-06-05 10:21:08 +0200

    fixed general

--- TrialTower/main.cpp
+++ TrialTower/main.cpp
@@ -434,7 +434,7 @@ int main(int argc, char* args[])
 							gameEnded = true;
 							titleScreen.checkScore(player.getMoney());
 						}
-					}
+					}		
 
 					//Render the map
 					lvlMap.renderAll();
